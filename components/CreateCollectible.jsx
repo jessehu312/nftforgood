186c47b5e5bc08e62ab8b2ec8285ab7c9204b1c5
aryanvichare <aryanvichare10@gmail.com>
2021-03-27 23:28:39 -0700

    add dropzone group hover

--- components/CreateCollectible.jsx
+++ components/CreateCollectible.jsx
@@ -79,16 +79,16 @@ const CreateCollectible = () => {
           </div>
           <div className="w-full">
             <div className="inline-block mb-32 w-full md:w-5/12">
-              <label className="w-full transition-colors flex flex-col items-center px-4 py-6 bg-transparent text-blue rounded-lg shadow-lg tracking-wide uppercase border-4 border-blue cursor-pointer hover:text-white border-dashed py-12">
+              <label className="group w-full transition-colors flex flex-col items-center px-4 py-6 bg-transparent text-blue rounded-lg shadow-lg tracking-wide uppercase border-4 border-blue hover:border-yellow-400 cursor-pointer hover:text-white border-dashed py-12">
                 <svg
-                  className="text-white w-8 h-8"
+                  className="transform-colors duration-150 ease-in-out group-hover:text-yellow-400 text-white w-8 h-8"
                   fill="currentColor"
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 20 20"
                 >
                   <path d="M16.88 9.1A4 4 0 0 1 16 17H5a5 5 0 0 1-1-9.9V7a3 3 0 0 1 4.52-2.59A4.98 4.98 0 0 1 17 8c0 .38-.04.74-.12 1.1zM11 11h3l-4-4-4 4h3v3h2v-3z" />
                 </svg>
-                <span className="text-white mt-2 text-base leading-normal">
+                <span className="transform-colors duration-150 ease-in-out group-hover:text-yellow-400 text-white mt-2 text-base leading-normal">
                   Select a file
                 </span>
                 <input
