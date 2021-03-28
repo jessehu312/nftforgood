c5b9b1746dd4f747ba745ce468935089dbc49c3e
jessehu312 <jessehu312@gmail.com>
2021-03-27 19:47:58 -0700

    Fixed empty data spacing

--- components/CollectibleCard.jsx
+++ components/CollectibleCard.jsx
@@ -5,7 +5,7 @@ import { useRouter } from 'next/router';
 const CollectibleCard = ({
   name,
   img,
-  fiatPrice,
+  fiatPrice = <br />,
   inStock,
   totalQuantity,
   id
@@ -15,7 +15,7 @@ const CollectibleCard = ({
     <a onClick={() => router.push(COLLECTIBLE_URL + id)}>
       <div
         data-name={name.split(' ')[0].toLowerCase()}
-        className="cursor-pointer group bg-white shadow-lg rounded-4xl p-7 pt-8 transform hover:scale-105 transition ease-in-out duration-300 hover:bg-white stroke-current text-black border-solid border-4 border-gray-300"
+        className="cursor-pointer h-88 group bg-white shadow-lg rounded-4xl p-7 pt-8 transform hover:scale-105 transition ease-in-out duration-300 hover:bg-white stroke-current text-black border-solid border-4 border-gray-300"
       >
         <img className="w-full mx-auto rounded-xl h-48 w-3/12" src={img} alt={name} />
         <h2 className="text-black text-1xl font-bold mt-4">{name}</h2>
