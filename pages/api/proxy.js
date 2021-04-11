866b99da7b4fd560ce636645ad3df370c515ed44
aryanvichare <aryanvichare10@gmail.com>
2021-04-11 16:34:21 -0700

    :rocket: Add Collectible to Chainlink
    Add Collectible to Chainlink

--- pages/api/proxy.js
+++ pages/api/proxy.js
@@ -4,7 +4,10 @@ export default async (req, res) => {
   const {
     query: { proxyRoute }
   } = req;
-  
-  const result = await axios.post(`${process.env.NEXT_PUBLIC_BACKEND_ENDPOINT}/${proxyRoute}`, req.body);
+
+  const result = await axios.post(
+    `${process.env.NEXT_PUBLIC_BACKEND_ENDPOINT}/${proxyRoute}`,
+    req.body
+  );
   res.json(result.data);
 };
